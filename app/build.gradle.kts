57050b0ca83025386634815d389a4c89afc872df
Aristide CORDONNIER <cordonnier.aris@gmail.com>
2024-12-15 23:09:18 +0100

    Ajustements
    Mise à jour des nom des fonts
    Implémentation des packages Firebase
    Test login local
    Création home_page.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -34,7 +35,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+
     buildFeatures {
+        viewBinding = true
         compose = true
     }
 }
@@ -56,4 +60,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.1.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
 }
--- app/src/main/java/com/safe/application/MainActivity.kt
+++ app/src/main/java/com/safe/application/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.safe.application
 
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputBinding
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import com.safe.application.databinding.LoginPageBinding
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,36 +21,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.safe.application.ui.theme.ApplicationTheme
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: LoginPageBinding
+
+
+    // Variables pour la connexion avec Firebase
+//    lateinit var auth : FirebaseAuth
+//    lateinit var database : FirebaseDatabase
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.login_page)
+
+        // Logique de connexion avec Firebase (plus tard pour l'instant c'est un test local)
+        binding = LoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+
+            if (binding.emailInput.text.toString() == "admin" && binding.passwordInput.text.toString() == "admin") {
+                Toast.makeText(this , "Login Successful" , Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this , "Login Failed" , Toast.LENGTH_SHORT).show()
             }
+
+
+        binding.forgotPassword.setOnClickListener {
+            Toast.makeText(this , "Forgot Password Clicked" , Toast.LENGTH_SHORT).show()
         }
+        })
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ApplicationTheme {
-        Greeting("Android")
-    }
-}
